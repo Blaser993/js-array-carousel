7f85a32094f77626a90d56b0a389b138d8121525
Blaser993 <corradodepinto.design@gmail.com>
2023-02-06 17:25:02 +0100

    FEAT cambio indice al click

--- script/myscript.js
+++ script/myscript.js
@@ -3,18 +3,50 @@ console.log("ciao")
 // MILESTONE 2
 // Al click dell’utente sulle frecce (una a sinistra e una a destra), il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente. Attenzione quando arriviamo all’ultima slide e clicchiamo sulla freccia destra, o viceversa se siamo sulla prima e clicchiamo sulla freccia sinistra.
 
+const caruselImg = ["./img/01.jpg",
+"./img/02.jpg",
+"./img/03.jpg",
+"./img/04.jpg",
+"./img/05.jpg"]
+
+
+let i
+i = caruselImg[0]
+console.log(i);
+
+
+
+document.getElementById("evaristo")
+
+let clickSinistro = evaristo.addEventListener("click", function(){
+    console.log("cambio alla precedente immagine");
+
+
+});
+
+
 document.getElementById("ernesto")
 
 let clickDestro = ernesto.addEventListener("click", function(){
-    console.log("cambio alla seguente immagine")
+    console.log("cambio alla seguente immagine");
+
+     
+
+    for (let i = 0; i < caruselImg.length -1; i++){
+        
+        
+
+        console.log(i);
+
+
+    }
+
+
+
+
 });
 
 
-document.getElementById("everisto")
-
-let clickSinistro = evaristo.addEventListener("click", function(){
-    console.log("cambio alla precedente immagine")
-});
 
 
 
