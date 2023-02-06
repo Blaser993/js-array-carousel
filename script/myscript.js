d9be296a023162e623db78204afb83d5ef174254
Blaser993 <corradodepinto.design@gmail.com>
2023-02-06 15:58:14 +0100

    REFACT

--- script/myscript.js
+++ script/myscript.js
@@ -1,1 +1,7 @@
 console.log("ciao")
+
+// MILESTONE 2
+// Al click dell’utente sulle frecce (una a sinistra e una a destra), il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente. Attenzione quando arriviamo all’ultima slide e clicchiamo sulla freccia destra, o viceversa se siamo sulla prima e clicchiamo sulla freccia sinistra.
+// MILESTONE 3
+// Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell’array fornito per generare il markup delle slide potete usare la concatenazione delle stringe, il template literal o il metodo createElement… a voi la scelta.
+// Nel generare il murkup fate attenzione che solo una delle 5 slide deve avere la classe active e risultare quindi visibile…
